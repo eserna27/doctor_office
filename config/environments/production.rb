f8ec3525897c086921c7b6f439763dc3a2e94b87
Emmanuel Serna <eserna27@gmail.com>
2017-12-11 17:26:30 -0600

    add defaul_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "doctar_office_#{Rails.env}"
   config.action_mailer.perform_caching = false
   # Setup the mailer config
+  config.action_mailer.default_url_options = { host: 'https://consultoriomedico.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
